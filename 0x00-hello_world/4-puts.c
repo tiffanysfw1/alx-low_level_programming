04d82eee44d311da207be6f5e416d191852fa4c7
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-08 04:51:18 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts('\"programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
