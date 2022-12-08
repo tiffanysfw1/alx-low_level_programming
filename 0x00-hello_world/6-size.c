1415711224832e778052bf075e99644bc1905917
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-08 06:43:48 -0800

    sizeofdata

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main -Entry  point
  * Return: Always 0 Success
@@ -11,8 +12,8 @@ int main(void)
 	long long int k;
 	float v;
 
-	printf("Size of int: %zu byte(s)\n", sizeof(b));
 	printf("Size of char: %zu byte(s)\n", sizeof(x));
+	printf("Size of int: %zu byte(s)\n", sizeof(b));
 	printf("Size of long int: %zu byte(s)\n", sizeof(y));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(k));
 	printf("Size of float: %zu byte(s)\n", sizeof(v));
