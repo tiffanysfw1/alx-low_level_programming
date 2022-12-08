23cc98de0961fbb10d2a461ef9a7fcc864329e85
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-08 05:06:22 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point
- * return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
