630d33a1a3767af8561ae0c6cd2406df748ae3b6
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-08 06:35:49 -0800

    sizeofdata

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+/**
+ * main -Entry  point
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int b;
+	char x;
+	long int y;
+	long long int k;
+	float v;
+
+	printf("Size of int: %zu byte(s)\n", sizeof(b));
+	printf("Size of char: %zu byte(s)\n", sizeof(x));
+	printf("Size of long int: %zu byte(s)\n", sizeof(y));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(k));
+	printf("Size of float: %zu byte(s)\n", sizeof(v));
+        return (0);
+}
