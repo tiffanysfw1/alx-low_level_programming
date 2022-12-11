16ecd2b34756d2afddaf88e327e89c7033a5f864
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-11 12:37:03 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - aphabets in reverse order
+ * Return: always zero
+ */
+int main(void)
+{
+	char k;
+
+	for(low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}	
