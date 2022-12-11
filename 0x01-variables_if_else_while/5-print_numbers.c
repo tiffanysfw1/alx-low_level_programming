032df36f6858942af60dcf6afe49721230cdf8ac
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-11 12:06:59 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - base 10 numbers
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int k;
+
+	for (k=0; k<=9; k++)
+		printf("%d", k;)
+			printf("\n");
+	return (0);
+}
