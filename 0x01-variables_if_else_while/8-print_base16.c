158b0d961b2b579e87f368eb2f41c4bf9dbf8970
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-11 13:03:10 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print numbers of base sixteen in lowecase 
+ * main - print numbers of base sixteen in lowecase
  * Return: always zero
  */
 int main(void)
@@ -9,9 +9,9 @@ int main(void)
 	int k;
 	int l;
 
-	for (k = '0'; k<= '9'; k++)
+	for (k = '0'; k <= '9'; k++)
 		putchar(k);
-	for (l = 'a'; l<= 'f'; l++)
+	for (l = 'a'; l <= 'f'; l++)
 		putchar(l);
 	putchar('\n');
 	return (0);
