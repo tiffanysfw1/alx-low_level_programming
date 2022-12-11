92443eb75b8aa90657538be6717037e9ea7455ed
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-11 08:55:49 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+        putchar(ch);
+	putchar('\n');
+	return (0);
+}
