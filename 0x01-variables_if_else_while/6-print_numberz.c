f8246d42c30bfea3c6e48de99f74c6d98c3e65ca
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-11 12:23:38 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print the base numbers using putchar
+ * Return: always zero
+ */
+int main(void)
+{
+	int a;
+
+	for (n= '0'; n < '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return 0;
+}
