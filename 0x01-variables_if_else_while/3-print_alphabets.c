a1e190aace6e9f82b1b272cbc23b00abaa0fcfd1
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-11 09:16:18 -0800

    lowerupper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet for both lower and uppercase
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	char ch;
+	char CH;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for(CH = 'a'; CH <= 'z'; CH++)
+		putchar(CH);
+	putchar('\n');
+	return (0);
+}
