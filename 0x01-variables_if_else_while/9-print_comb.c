5322da47f2d0cc1f98bcfe0ea75abef3ecd7be14
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-11 13:10:47 -0800

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h
+
+/**
+ * main - print single digits
+ * Return: always zero
+ */
+int main(void)
+{
+	int z;
+
+	for (z = '0'; z <= '9'; z++)
+	{
+		putchar(z);
+		if (z == '9')
+			break;
+		putchar(',');
+			putchar(' ');
+	}
+	putchar('\');
+	return (0);
