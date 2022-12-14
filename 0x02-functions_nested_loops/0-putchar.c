53356e944082e23b1e0a7dac110a6ade7a8f7c55
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-14 15:22:46 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print putchar
+ * Return: always zero
+ *
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
