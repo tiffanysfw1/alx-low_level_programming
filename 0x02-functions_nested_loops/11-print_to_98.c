5de032a3b41ff754aa02707f4ac59cd00a5b1c90
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-15 06:44:59 -0800

    from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_98 - check number from n to 98
+ * @n: integer n
+ * Return: none
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for(; n<=98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
