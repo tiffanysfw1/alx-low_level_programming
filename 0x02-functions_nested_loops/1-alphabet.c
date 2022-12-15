ed1d49e49b6bc0e812198ec06a62798dda2a9290
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-14 16:18:57 -0800

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print lowecase alphabet letters
+ * Return: always zero
+ *
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x<= 'a'; a++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
