a67a2caa1afc8552adb1860043a042db0736b7dc
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-14 17:35:18 -0800

    alphabet10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char k;
+	int no;
+
+	for (no = 0; no <= 9; no++)
+	{
+		for (k = 'a'; k <= 'z'; k++)
+			_putchar(k);
+		_putchar('\n');
+	}
+}
