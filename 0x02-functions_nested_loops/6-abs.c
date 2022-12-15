532de37a80fd995d23643a2eee3c0ee07bac1f5c
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-14 18:39:08 -0800

    integer value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value of integer
+ * @abs - integer
+ * Return: absolutr value of an integer
+ */
+int _abs(int)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
