6e4e3e81ed372bbca6d80df09f0fd58e183a41eb
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-14 19:09:28 -0800

    minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print minutes of the day
+ * h = hour, m = minutes
+ * for loop breaks
+ * Return: 24 hourclock
+ */
+void jack_bauer(void)
+{
+	int hours, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
