2914bec2e5ed38d282e4f0beb85d091e7020a7b4
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-16 03:45:31 -0800

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,5 +7,13 @@
  */
 int main(void)
 {
+	unsigned long int j = 3, k = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
 	return (0);
 }
