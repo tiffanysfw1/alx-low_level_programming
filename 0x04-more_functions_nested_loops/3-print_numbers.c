111ae5c926b5136b85975bb12e757ddd86b5fc5f
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-16 05:04:37 -0800

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return:0
+ */
+void print_numbers(void)
+{
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
