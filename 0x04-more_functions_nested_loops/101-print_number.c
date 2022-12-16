b9137c12480dafe5382dad4f4cc82f2649a85537
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-16 07:50:00 -0800

    nnumbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,33 +4,33 @@ void print_integer(int m);
 /**
  * print_number - function that prints an integer
  * @n: An integer
- * Return: nothing
+ * Return: Nothing
  */
 void print_number(int n)
 {
 	if (n == 0)
 		_putchar('0');
-		else if (n < 0)
-		{
-			_putchar('-');
-			print_integer(n * -1);
-		}
-		else
-			print_integer(n);
-}
-	
-	/**
-	 * print_integer - A function to print n
-	 * @m: an input unsigned integre
-	 * Return: Nothing
-	 */
-	void print_integer(int m)
+	else if (n < 0)
 	{
-		int i = 1000000000;
-
-		for (; i >= 1; i /= 10)
-			if (m / i != 0)
-			{
-				_putchar((m / i) % 10 + '0');
-			}
+		_putchar('-');
+		print_integer(n * -1);
 	}
+	else
+		print_integer(n);
+}
+
+/**
+ * print_integer - function to print n
+ * @m: an input integer
+ * Return: Nothing
+ */
+void print_integer(int m)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
+}
