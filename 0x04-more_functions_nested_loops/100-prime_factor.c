017838b9c978be7ac85b99dac617c2048582d66a
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-16 03:53:32 -0800

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print the largest prime factor 612852475143
- * Return: 0
- *
+ * main - entry point
+ * Return: Always (0)
  */
 int main(void)
 {
 	unsigned long int j = 3, k = 612852475143;
 
-	for (; i < 12057; i += 2)
+	for (; j < 12057; j += 2)
 	{
-		while (n % i == 0 && n != i)
-			n /= i;
+		while (k % j == 0 && k != j)
+			k /= j;
 	}
-	printf("%lu\n", n);
+	printf("%lu\k", k);
 	return (0);
 }
