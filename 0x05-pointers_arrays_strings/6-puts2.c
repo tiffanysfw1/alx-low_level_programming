685de81b6126a6b1b9527855db75fc9a3bbccfc4
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-19 04:59:53 -0800

    half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function that print string starting with the first character
+ * @str: string
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len -= 1;
+
+	for (; i<= len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
