f25d9269cea58629197670324eb2e75a9f0024f8
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-19 12:52:07 +0300

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - set value to char
+ * @s: double pointer
+ * @to: pointer
+ * Return: always 0
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
