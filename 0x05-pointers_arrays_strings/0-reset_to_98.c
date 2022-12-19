01038d31ad431cadbe6f668aeeaab55e8b5cb3ef
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-18 22:49:55 -0800

    battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to print value to the parameter
+ * @n: integer input pointer
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
