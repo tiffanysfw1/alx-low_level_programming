ece24dd6dab105a49314eabd41c506b8d5d722e7
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-19 14:03:39 +0300

    function to print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to prints  a string
+ * @s: string
+ * Return: always zero
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s)
+_puts_recursion(s + 1)
+}
+puchar('\n');
+}
