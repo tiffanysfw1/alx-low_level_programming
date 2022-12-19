0d967815c0fad46402d3ee1b838e1734e36321e7
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-19 05:32:17 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string
+ * @dest: pointer
+ * @src: pointer
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
+}
