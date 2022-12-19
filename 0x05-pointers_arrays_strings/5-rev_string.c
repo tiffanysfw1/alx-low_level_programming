4b9c270198a7b68f019ced109a90f6e7b5dacda7
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-19 04:45:45 -0800

    revstring

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - function that print string in reverse
+ * @s: string
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int i = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len --)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
+}
