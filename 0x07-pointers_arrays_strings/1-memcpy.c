939eee60efab2527737253dbef6a56b798553298
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-19 11:55:12 +0300

    memcopy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-  char *start = dest;
+char *start = dest;
 
-  while (n--)
-  {
-    *dest = *src;
-    src++;
-    dest++;
-  }
-  return (start);
+while (n--)
+{
+*dest = *src;
+src++;
+dest++;
+}
+return (start);
 }
