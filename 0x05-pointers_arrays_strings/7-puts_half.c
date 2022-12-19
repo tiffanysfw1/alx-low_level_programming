8e3313965c3717f9887e4b710388c56909d3bb46
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-19 05:08:20 -0800

    winning

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * puts_half - function that print half of a string
+ * @str: string
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}#include "main.h"
+
+/**
+ * puts_half - function that print half of a string
+ * @str: string
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}#include "main.h"
+
+/**
+ * puts_half - function that print half of a string
+ * @str: string
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
