fb73668ee3cc5cc180fbc9d7093d8c22ab4a894b
tiffanysfw <tiffyjemohappy19@gmail.com>
2022-12-22 21:11:34 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: string
+ * @n: string
+ * Return: Always  0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *vmp = dest, *bvn = src;
+	int gimp = 0; i = 0;
+
+	while (*src)
+	{
+		gimp++;
+		src++;
+	}
+
+	while (*dest)
+	{
+		dest++;
+	}
+
+	if (n > gimp)
+	{
+		n = gimp;
+	}
+
+	src = bvn;
+
+	for (; i < n; i++;)
+	{
+		*dest++ = *src++;
+	}
+
+	*dest = '\0';
+	return (temp);
+}
