e21b48492f0cd28a5204d0d2c67711166cd8a986
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-07 01:06:18 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,10 +8,13 @@
 int factorial(int n)
 {
   int result = n;
-  return (-1);
+  
+  if (n < 0)
+    return (-1);
   
   else if (n >= 0 && n <= 1)
     return (1);
+  
   result *= factorial(n - 1);
   
   return (result);
