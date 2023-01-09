684ce95cec1437402f32fe8e255493bc8d549e73
tiffanysfw1 <tiffiejemosop@gmail.com>
2023-01-09 13:49:52 -0800

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function to multiply numbers in form of integers
+ * @a: number of arguments
+ * @b: pointer to arguments
+ * Return: sucess
+ */
+int main(int a, char **b)
+{
+	if (a < 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(*(b + 1)) * atoi(*b + 2)));
+	return (0);
+}
