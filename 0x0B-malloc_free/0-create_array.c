f9d9d4e2ba389c4f389e58098965fd95b9a8c56d
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-11 17:38:06 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,25 @@
-#include "main.h"                                                                                                                       
-#include <stdlib.h>                                                                                                                     
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create arrays of char dynamically
+ * @size: size
+ * @c: always zero
+ */
+char *create_array(unsigned int size, char c)
+{
+        char *arrays;
+        unsigned int b;
+
+        if (size == 0)
+        return (NULL);
+
+        arrays = (char *)malloc(sizeof(char) * size);
+        if (arrays == NULL)
+        return (NULL);
                                                                                                                                         
-/**                                                                                                                                     
- * create_array - create arrays of char dynamically                                                                                     
- * @size: size                                                                                                                          
- * @c: always zero                                                                                                                      
- */                                                                                                                                     
-char *create_array(unsigned int size, char c)                                                                                           
-{                                                                                                                                       
-        char *arrays;                                                                                                                   
-        unsigned int b;                                                                                                                 
-                                                                                                                                        
-        if (size == 0)                                                                                                                  
-                return (NULL);                                                                                                          
-                                                                                                                                        
-        arrays = (char *)malloc(sizeof(char) * size);                                                                                   
-        if (arrays == NULL)                                                                                                             
-        return (NULL);                                                                                                                  
-                                                                                                                                        
-        for (b = 0; b < size; b++)                                                                                                      
-                arrays[b] = c;                                                                                                          
-                                                                                                                                        
-        return (arrays);                                                                                                                
-}                                                                                                                                     
+        for (b = 0; b < size; b++)
+                arrays[b] = c;
+
+        return (arrays);
+}
