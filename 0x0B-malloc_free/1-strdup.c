f855c67f8981ea3a3a2b00d307028b7f4d6635fe
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-12 15:48:45 +0300

    Create 1-strdup.c
    pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - function that returns pointer
+* @str: string
+* Return: always zero
+*/
+char *_strdup(char *str)
+{
+char *duplicate;
+int i, len = 0;
+if (str == NULL)
+return (NULL);
+while (str[len] != '\0')
+len++;
+duplicate = (char *)malloc((size of(char) * len) + 1)
+if (duplicate == NULL)
+return (NULL);
+for (i = 0; i < len; i++;
+duplicate[i] = str[i];
+duplicate[len] = '\0';
+return (duplicate);
+}
