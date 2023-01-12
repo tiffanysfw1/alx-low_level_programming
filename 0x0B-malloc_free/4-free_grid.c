b2a9af75970c7ec1805436e89a3e0a6b1d54f6c2
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-12 16:35:31 +0300

    Create 4-free_grid.c
    free dimension arrays

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the grid
+ * @height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+int k;
+for (k = 0; k < height; i++)
+free(grid[k]);
+free(grid);
+}
