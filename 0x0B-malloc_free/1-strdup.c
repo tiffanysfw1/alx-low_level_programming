a9ea979956cff88ea59ca4413cef03916eaf708f
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-12 15:55:46 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ if (str == NULL)
 return (NULL);
 while (str[len] != '\0')
 len++;
-duplicate = (char *)malloc((sizeof(char) * len) + 1);
+duplicate = (char *)malloc((sizeof(char) * len) +1);
 if (duplicate == NULL)
 return (NULL);
 for (i = 0; i < len; i++)
