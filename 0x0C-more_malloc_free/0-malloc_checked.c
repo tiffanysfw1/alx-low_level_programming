fe2d5360fde1d228d1888c8c22f080331f66a8c4
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-14 22:56:12 +0300

    Create 0-malloc_checked.c
    memory allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* malloc_checked - function that allocate memory using malloc
+* @b: unsigned int
+* Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+void *j
+if (j == NULL)
+_putchar(98);
+return (j);
+}
