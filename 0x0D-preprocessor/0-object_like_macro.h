4f10b650a6aa5a3d85791e045abc10618cc3ae7e
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-17 11:27:58 +0300

    Create 0-object_like_macro.h
    macros

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+* main - check the code
+* Return: always zero
+*/
+#define SIZE 1024
+#define int 98
+int j, k;
+j = SIZE;
+k = int;
+return (0);
