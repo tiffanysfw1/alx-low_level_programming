ccf86d4c58be0499acf1c9d52c311cd6753371bb
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-17 14:12:14 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,9 @@
-#ifndef "0-object_like_macro.h"
-#define "0-object_like_macro.h"
-#include <stdio.h>
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 /**
 * main - check the code
 * Return: always zero
 */
 #define SIZE 1024
-{
-return (SIZE);
-}
+#endif
