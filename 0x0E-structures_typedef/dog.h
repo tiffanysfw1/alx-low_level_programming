2e112ca8a63d7ed32618cdf3366bf130bf6bc0af
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-17 18:33:36 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,14 @@
 #define DOG_H
 #ifndef DOG_H
-
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
