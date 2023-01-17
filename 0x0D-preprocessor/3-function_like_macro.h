173df3f3a893093e0c8a71c143fd48d712539a78
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-17 15:40:11 +0300

    Create 3-function_like_macro.h
    function like macros

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+* main - function like macro to give absolute value
+* Return: value
+*/
+#define ABS(x) ((x) < (0) ? (-x) : (x))
+#endif
