8da27c4258264d535fde21e350e7b87082530d43
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-21 21:12:20 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,8 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 if (array == NULL || action == NULL)
 return;
-while (size > 0)
-size--;
+while (size-- > 0)
 {
 action(*array)
 array++;
