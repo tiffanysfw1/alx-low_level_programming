79f3410dd95ed66c945ce5fea4dea3d0728b51cb
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-21 21:54:32 +0300

    Create 3-op_functions.c
    calculator program

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+*op_add - function that returns the sum 
+*@a: number 1
+*@b: number 2
+*Return: sum
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+*op_sub - function that returns subtraction
+*@a: number 1
+*@b: number 2
+*Return: subtraction
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+*op_mul - function that returns multiplication
+*@a: number 1
+*@b: number 2
+*Return: multiplication
+*/
+int op_mul(int a, int b)
+{
+return ( a * b);
+}
+/**
+*op_div - function that returns division
+*@a: number 1
+*@b: number 2
+*Return: division
+*/
+int op_div(int a, int b);
+{
+return (a / b);
+}
+/**
+*op_mod - function that returns remainder
+*@a: number 1
+*@b: number 2
+*Return: remainder
+*/
+int int op_mod(int a, int b)
+{
+return (a % b);
+}
