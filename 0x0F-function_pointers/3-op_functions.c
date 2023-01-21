6da5f7aabc06af2bf9fd402c235b9fb223e800db
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-21 23:04:39 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,9 @@
 #include "3-calc.h"
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
 /**
 *op_add - function that returns the sum 
 *@a: number 1
