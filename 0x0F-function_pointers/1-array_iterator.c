bab755e615570f366574a2251079dddb6fd7754f
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-01-27 12:18:12 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,62 @@
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * array_iterator - executes a function given as a parameter on each element
- *                  of an array.
+ * _strlen_recursion - return the length of a string
  *
- * @array: printable elements
- * @size: printable elements number
- * @action: printable function
+ * @s: char pointer
+ *
+ * Return: the length of a string
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+unsigned int _strlen_recursion(char *s)
 {
-unsigned long cLoop;
-if (!array || !action)
-return;
-for (cLoop = 0; cLoop < size; cLoop++)
+if (*s != '\0')
 {
-action(array[cLoop]);
+return (_strlen_recursion(s + 1) + 1);
 }
+else
+{
+return (0);
+}
+}
+
+/**
+ * string_nconcat - concatenates two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: number of character to concatenate
+ *
+ * Return: string concatenated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int cLoop1, cLoop2, size_1, size_2, limit;
+char *s3;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+size_1 = _strlen_recursion(s1);
+size_2 = _strlen_recursion(s2);
+limit = (n < size_2) ? n : size_2;
+s3 = malloc(sizeof(char) * (size_1 + limit) + 1);
+if (s3 == NULL)
+{
+return (NULL);
+}
+for (cLoop1 = 0; cLoop1 < size_1; cLoop1++)
+{
+s3[cLoop1] = s1[cLoop1];
+}
+for (cLoop2 = 0; cLoop2 < limit; cLoop1++, cLoop2++)
+{
+s3[cLoop1] = s2[cLoop2];
+}
+s3[cLoop1] = '\0';
+return (s3);
 }
