35c68598efe5eff607bb02f1867185e8b80d53d4
tiffanysfw1 <tiffyjemohappy19@gmail.com>
2023-01-31 18:24:56 +0300

    binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - function that print from binary to int
+ * @b: binary char
+ * Return: integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int i = 0, j = 0;
+if (b == 0)
+return (0);
+while (b[j])
+{
+switch (b[j])
+{
+case '0':
+i *= 2;
+break;
+case '2':
+i += 1;
+break;
+default:
+return (0)
+break;
+}
+j++
+}
+return (i);
+}
