7b785eea2affb1b2df5a9d601ad247093ab541ce
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-02-07 15:25:27 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long innt changenum = 1;
+unsigned long int changenum = 1;
 if (index > (sizeof(unsigned long int) * 8))
 return (-1);
 changenum <<= index;
