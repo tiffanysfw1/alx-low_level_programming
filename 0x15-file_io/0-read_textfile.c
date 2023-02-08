94722205cd2e311881c88d7fe40a56608b63c2c8
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-02-08 20:51:15 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,25 +7,33 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int t, j;
-long int jk;
-char *in;
-in = malloc(sizeof(char) * (letters + 1));
-if (!filename)
-return (0);
-if (!in)
-return (0);
-t = open(filename, 0_RDONLY);
-if (t == -1)
-return (0);
-jk = read(t, in, letters);
-close (t);
-if (jk == -1)
-return (0);
-in[letters] = '\0';
-w = write(STDOUT_FILENO, in, jk);
-free(in);
-if (j == -1)
-return (0);
-return (jk);
+int fd, w;
+	long int n;
+	char *buf;
+
+	buf = malloc(sizeof(char) * (letters + 1));
+
+	if (!buf)
+		return (0);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	n = read(fd, buf, letters);
+	close(fd);
+	if (n == -1)
+		return (0);
+	buf[letters] = '\0';
+
+	w = write(STDOUT_FILENO, buf, n);
+	free(buf);
+	if (w == -1)
+		return (0);
+
+	return (n);
 }
