8ffe5a3315a0349e9401f1b7e05e7baf146b6534
tiffanysfw1 <122115043+tiffanysfw1@users.noreply.github.com>
2023-02-08 20:26:39 +0300

    Create main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EXIT_WRONG_PARAMETER 97
+#define EXIT_READ_ERROR 98
+#define EXIT_WRITE_ERROR 99
+#define EXIT_CLOSE_ERROR 100
+#define BUFFER_SIZE 1024
+#define DEFAULT_WRITE_RIGHTS 0664
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
